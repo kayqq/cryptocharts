5f6849f5489ea00ebc28cb0a4ed323254e73d6c4
Kenny Quan <quan_kenny@yahoo.com>
2019-04-04 03:28:10 -0700

    Updates

---
+++ precache-manifest.59fd6061b0b5611c46c7ed7e39d50912.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "adaec33d4cc4a51c82994ee393ae1dc8",
+    "url": "/static/media/view_show_icon_hover_3.adaec33d.svg"
+  },
+  {
+    "revision": "fbb0ae0cb609959fdee4",
+    "url": "/static/css/main.7f1f6417.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ca62e39852127a3093f",
+    "url": "/static/js/2.54931fd8.chunk.js"
+  },
+  {
+    "revision": "d189a840367c6cb7cd822c06cd67fa92",
+    "url": "/static/media/delete_coin_icon.d189a840.svg"
+  },
+  {
+    "revision": "5df8c2ff2d42e78ec97f1abeca8dbf1f",
+    "url": "/static/media/delete_coin_icon_hover.5df8c2ff.svg"
+  },
+  {
+    "revision": "edc23fdfd0345bc994ef22e8261e0007",
+    "url": "/static/media/view_show_icon_3.edc23fdf.svg"
+  },
+  {
+    "revision": "fbb0ae0cb609959fdee4",
+    "url": "/static/js/main.253e7ec3.chunk.js"
+  },
+  {
+    "revision": "4e7ebf0f0545b370fb421ada9f67c88c",
+    "url": "/static/media/view_hidden_icon.4e7ebf0f.svg"
+  },
+  {
+    "revision": "ed4299992e429ff1870595af55114d12",
+    "url": "/static/media/add_coin_icon.ed429999.svg"
+  },
+  {
+    "revision": "153676cef7bcd2efb256a2a3c3352bff",
+    "url": "/static/media/view_hidden_icon_hover.153676ce.svg"
+  },
+  {
+    "revision": "818fed0ae7a1b5922fb49c4208646118",
+    "url": "/static/media/add_coin_icon_hover.818fed0a.svg"
+  },
+  {
+    "revision": "afa39974c5b6709de29b0f7d877e5a1e",
+    "url": "/static/media/coin_added_icon.afa39974.svg"
+  },
+  {
+    "revision": "3eb42bb3e3402599919edb102937aadb",
+    "url": "/static/media/search_icon.3eb42bb3.svg"
+  },
+  {
+    "revision": "3783be6288549fa8c53abad3f02d24d3",
+    "url": "/index.html"
+  }
+];
